0657e7f91df40fa7ee1a0924009218cdcac7dab3
tomohiro.zoda <tomohiro.zoda@dena.com>
2016-01-01 13:40:03 +0900

    Playerモデルをシングルトンで追加

--- zo-tune/PlayViewController.swift
+++ zo-tune/PlayViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
+import Foundation
+import MediaPlayer
 
 class PlayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // DEBUG: response のチェックとかこの辺でやる
+        var albumsQuery: MPMediaQuery = MPMediaQuery.albumsQuery()
+        println(albumsQuery)
     }
     
     override func didReceiveMemoryWarning() {
---
+++ zo-tune/Player.swift
@@ -0,0 +1,18 @@
+//
+//  Player.swift
+//  zo-tune
+//
+//  Created by Tomohiro Zoda on 2016/01/01.
+//  Copyright (c) 2016年 Tomohiro Zoda. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    static let PLAYER = Player()
+
+    // MARK: Properties
+
+    // MARL: Initialization
+    init() {}
+}
